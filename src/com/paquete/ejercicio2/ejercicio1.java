92db7a42745cea5d3eb6c6b1171affd67219dc04
Nuria Garcia Cobas <ngarciacobas@danielcastelao.org>
2021-10-15 08:42:31 +0200

    cambios outprint

--- src/com/paquete/ejercicio2/ejercicio1.java
+++ src/com/paquete/ejercicio2/ejercicio1.java
@@ -2,8 +2,8 @@ package com.paquete.ejercicio2;
 
 public class ejercicio1 {
 
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
 }
 
-    public static void main(String[] args) {
-
-    }
